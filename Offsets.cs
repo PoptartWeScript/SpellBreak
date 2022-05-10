6f17e712f7539f3974b4be497bd68c35fc67c3d6
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2022-05-10 10:18:50 -0500

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -1,59 +1,59 @@
-﻿using System;
+using System;
 namespace SpellBreak
 {
 
-    public class Offsets
+public class Offsets
 
-    {
-        public static Int64 UWorld = 0x738BEB8;
-        public static Int64 GNames = 0x7404700;
+{
+   public static Int64 UWorld = 0x738BEB8;
+   public static Int64 GNames = 0x7404700;
 
-        public class UE
-        {
+public class UE
+{
 
-            public class UWorld
-            {
-                public static Int64 ULevel = 0x38;
-                public static Int64 OwningGameInstance = 0x170;
-            }
+   public class UWorld
+{
+       public static Int64 ULevel = 0x38;
+       public static Int64 OwningGameInstance = 0x170;
+}
 
-            public class ULevel
-            {
-                public static Int64 AActors = 0xA0;
-                public static Int64 AActorsCount = 0xA8;
-            }
+   public class ULevel
+{
+       public static Int64 AActors = 0xA0;
+       public static Int64 AActorsCount = 0xA8;
+}
 
-            public class UGameInstance
-            {
-                public static Int64 LocalPlayers = 0x40;
-            }
+   public class UGameInstance
+{
+       public static Int64 LocalPlayers = 0x40;
+}
 
-            public class UPlayer
-            {
-                public static Int64 PlayerController = 0x38;
-            }
+   public class UPlayer
+{
+       public static Int64 PlayerController = 0x38;
+}
 
-            public class APlayerController
-            {
-                public static Int64 AcknowledgedPawn = 0x3D0;
-                public static Int64 PlayerCameraManager = 0x3E8;
-            }
+   public class APlayerController
+{
+       public static Int64 AcknowledgedPawn = 0x3D0;
+       public static Int64 PlayerCameraManager = 0x3E8;
+}
 
-            public class APawn
-            {
-                public static Int64 PlayerState = 0x368;
-            }
-            public class AActor
-            {
-                public static Int64 USceneComponent = 0x168;
-                public static Int64 tempVec = 0x17C;
-            }
+   public class APawn
+{
+       public static Int64 PlayerState = 0x368;
+}
+   public class AActor
+{
+       public static Int64 USceneComponent = 0x168;
+       public static Int64 tempVec= 0x17C;
+}
 
-            public class APlayerCameraManager
-            {
-                public static Int64 CameraCachePrivate = 0x1A90;
-            }
+   public class APlayerCameraManager
+{
+       public static Int64 CameraCachePrivate = 0x1A90;
+}
 
-        }
-    }
+}
+}
 }
